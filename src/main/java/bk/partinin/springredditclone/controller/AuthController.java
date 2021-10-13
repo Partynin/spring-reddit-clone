cb8ab01af8b5aa978f2e790c791fb2a72fb2996b
Konstantin <partinin@bk.ru>
2021-10-13 17:12:07 +0400

    Add JwtFilter and Subreddit controller and service

--- src/main/java/bk/partinin/springredditclone/controller/AuthController.java
+++ src/main/java/bk/partinin/springredditclone/controller/AuthController.java
@@ -1,5 +1,7 @@
 package bk.partinin.springredditclone.controller;
 
+import bk.partinin.springredditclone.dto.AuthenticationResponse;
+import bk.partinin.springredditclone.dto.LoginRequest;
 import bk.partinin.springredditclone.dto.RegisterRequest;
 import bk.partinin.springredditclone.service.AuthService;
 import lombok.AllArgsConstructor;
@@ -24,5 +26,10 @@ public class AuthController {
     public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
         return new ResponseEntity<>("Account Activated Successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
     }
 }
--- src/main/java/bk/partinin/springredditclone/service/AuthService.java
+++ src/main/java/bk/partinin/springredditclone/service/AuthService.java
@@ -1,5 +1,7 @@
 package bk.partinin.springredditclone.service;
 
+import bk.partinin.springredditclone.dto.AuthenticationResponse;
+import bk.partinin.springredditclone.dto.LoginRequest;
 import bk.partinin.springredditclone.dto.RegisterRequest;
 import bk.partinin.springredditclone.exceptions.SpringRedditException;
 import bk.partinin.springredditclone.model.NotificationEmail;
@@ -7,7 +9,12 @@ import bk.partinin.springredditclone.model.User;
 import bk.partinin.springredditclone.model.VerificationToken;
 import bk.partinin.springredditclone.repository.UserRepository;
 import bk.partinin.springredditclone.repository.VerificationTokenRepository;
+import bk.partinin.springredditclone.security.JwtProvider;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +31,8 @@ public class AuthService {
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final MailService mailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtProvider jwtProvider;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -48,9 +57,7 @@ public class AuthService {
         VerificationToken verificationToken = new VerificationToken();
         verificationToken.setToken(token);
         verificationToken.setUser(user);
-
         verificationTokenRepository.save(verificationToken);
-
         return token;
     }
 
@@ -63,8 +70,17 @@ public class AuthService {
     @Transactional
     public void fetchUserAndEnable(VerificationToken verificationToken) {
         String username = verificationToken.getUser().getUsername();
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new SpringRedditException("User not found with name - " + username));
+        User user = userRepository.findByUsername(username).orElseThrow(() ->
+                new SpringRedditException("User not found with name - " + username));
         user.setEnabled(true);
         userRepository.save(user);
+    }
+
+    public AuthenticationResponse login(LoginRequest loginRequest) {
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authenticate);
+        String token =  jwtProvider.generateToken(authenticate);
+        return new AuthenticationResponse(token, loginRequest.getUsername());
     }
 }
