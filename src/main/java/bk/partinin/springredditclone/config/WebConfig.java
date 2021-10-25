3c0d0d0ddd3c3d55f7eed8a4d54b46db2f7cc442
Konstantin <partinin@bk.ru>
2021-10-25 20:10:06 +0400

    Add cross registry policy

--- src/main/java/bk/partinin/springredditclone/config/WebConfig.java
+++ src/main/java/bk/partinin/springredditclone/config/WebConfig.java
@@ -3,8 +3,10 @@ package bk.partinin.springredditclone.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/* This class for exclude error - CORS policy */
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -18,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       registry.addResourceHandler("swagger-ui.html")
+               .addResourceLocations("classpath:/META-INF/resources/");
+
+       registry.addResourceHandler("/webjars/**")
+               .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
